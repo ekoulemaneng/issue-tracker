bf538195210f8b60253682e0704485256c57aa6d
ekoulemaneng <ekoulemaneng@gmail.com>
2020-06-26 17:24:28 +0100

    fifth commit

--- app.js
+++ app.js
@@ -141,52 +141,6 @@ app.get('/api/issues/edimocms', (req, res) => {
   }
 });
 
-/*
-app.post('/api/exercise/new-user', (req, res) => {
-  let username = req.body.username;
-  let user = new User({'username': username, 'log': []});
-  user.save((err, newUser) => {
-    if (err) res.send('Username already taken');
-    res.json({"_id": newUser['_id'], "username": newUser['username']});
-  });
-});
-
-app.post('/api/exercise/add', (req, res) => {
-  let userId = req.body.userId;
-  let description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date ? (new Date(req.body.date)).toDateString() : (new Date()).toDateString();
-  User.findById(userId, (err, user) => {
-    if (err) res.send('No user is registered with this userId in the database');
-    else {
-      user['log'].push({"description": description, "duration": duration, "date": date});
-      user.save((err, user) => res.json({"userId": user['_id'], "username": user['username'], "description": description, "duration": parseInt(duration), "date": date}));}
-  })
-});
-
-app.get('/api/exercise/log', (req, res) => {
-  let userId = req.query.userId;
-  let from = req.query.from ? req.query.from : -8640000000000000;
-  let to = req.query.to ? req.query.to : 8640000000000000;
-  let limit = parseInt(req.query.limit);
-  User.findById(userId, (err, user) => {
-    if (err) res.send('No user is registered with this userId in the database');
-    else {
-      let log = user['log'].filter(obj => ((new Date(obj['date'])) >= (new Date(from))) && ((new Date(obj['date'])) <= (new Date(to))));
-      if (limit < log.length) log = log.slice(0, limit);
-      res.json({"userId": user['_id'], "username": user['username'], "count": log.length, "log": log});
-    }
-  })
-});
-
-app.get('/api/exercise/users', (req, res) => {
-  User.find({}, 'username',(err, users) => {
-    if (err) res.send('There are no user');
-    res.json(users);
-  });
-});
-*/
-
 app.use((err, req, res, next) => res.status(500).send('Something broke!'));
 
 app.use((req, res, next) => res.status(404).send('Sorry cant find that!'));
